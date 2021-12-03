2cf88bb981702048977b3505e9b6c4ab7c9ae9cb
Aarrez <aaronmhj01@outlook.com>
2021-12-03 16:43:31 +0100

    Line Animation curves

--- Assets/Aaron Floder/Scripts/LineScript.cs
+++ Assets/Aaron Floder/Scripts/LineScript.cs
@@ -3,16 +3,18 @@ using UnityEngine;
 
 public class LineScript : MonoBehaviour
 {
-    private Rigidbody2D rig2d;
-
     private DistanceJoint2D distJoint2d;
-
-    private Transform boat;
+    private float reelTime;
 
     [Header("Speed when reeling up and down")]
     [SerializeField] private float reelUp = 0.02f;
 
     [SerializeField] private float reelDown = 0.04f;
+
+    [Header("Animation reel curves")]
+    [SerializeField] private AnimationCurve reelUpCurve;
+
+    [SerializeField] private AnimationCurve reelDownCurve;
 
     private float inputValueY = 0f;
 
@@ -20,9 +22,7 @@ public class LineScript : MonoBehaviour
 
     private void Awake()
     {
-        rig2d = GetComponent<Rigidbody2D>();
         distJoint2d = GetComponentInParent<DistanceJoint2D>();
-        boat = GetComponentInParent<BoatScript>().transform;
     }
 
     private void OnEnable()
@@ -53,15 +53,23 @@ public class LineScript : MonoBehaviour
             case 0:
                 if (distJoint2d.distance < 22)
                     distJoint2d.distance += Time.fixedDeltaTime * 2;
+                reelTime = 0;
                 break;
 
             case 1:
-                distJoint2d.distance -= reelUp;
+
+                distJoint2d.distance -= reelUpCurve.Evaluate(reelTime);
+                reelTime += reelUp;
+                reelTime = Mathf.Clamp(reelTime, 0, 2);
                 break;
 
             case -1:
                 if (distJoint2d.distance < 22)
-                    distJoint2d.distance += reelDown;
+                {
+                    distJoint2d.distance += reelDownCurve.Evaluate(reelTime);
+                    reelTime -= reelDown;
+                    reelTime = Mathf.Clamp(reelTime, 0, 2);
+                }
                 break;
         }
     }
