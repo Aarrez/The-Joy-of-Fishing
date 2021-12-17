42b1847d4fe66df7e6034e43045a58f07a868dc2
Leo Asplund <theleolevito@gmail.com>
2021-12-17 16:31:25 +0100

    Added controlpanelbutton and GoFishButton

--- Assets/Scripting/EnableOnStartupScript.cs
+++ Assets/Scripting/EnableOnStartupScript.cs
@@ -9,10 +9,13 @@ public class EnableOnStartupScript : MonoBehaviour
     public GameObject PauseCanvas;
     public GameObject CallButton;
     public GameObject GoFishButton;
+    public GameObject ControlPanel;
+    bool controlPanelBool;
     void Start()
     {
         CreatePauseMenu.SetActive(true);
         GetC = CreatePauseMenu.GetComponent<CreatePauseMenuScript>();
+        ControlPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -41,5 +44,26 @@ public class EnableOnStartupScript : MonoBehaviour
         }
     }
 
+    public void ClickToEnableControlPanel()
+    {
+        if(controlPanelBool == false)
+        {
+            EnableControlPanel();
+        }
+        else if(controlPanelBool == true)
+        {
+            DisableControlPanel();
+        }
+    }
+    public void EnableControlPanel()
+    {
+        ControlPanel.SetActive(true);
+        controlPanelBool = true;
+    }
 
+    public void DisableControlPanel()
+    {
+        ControlPanel.SetActive(false);
+        controlPanelBool = false;
+    }
 }
