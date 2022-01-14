6a1d74c072951b0c894d46cd0cb792352cc6c2a2
Aaron Marklund <aaronmhj01@outlook.com>
2022-01-14 15:01:36 +0100

    FishSpawner Prefabs, Specific graphs of levels
    Fish now have a specific graph where they rome around. This means that sharks can no longer come to the surface.

--- Assets/Scripts/FishSpawner.cs
+++ Assets/Scripts/FishSpawner.cs
@@ -6,11 +6,12 @@ public class FishSpawner : MonoBehaviour
 
     public Transform spawnerPoint;
 
-    private GameObject gameBound;
-
     private Vector2 RandTarget = Vector2.zero;
 
     [SerializeField] private float spawnRadius = 10f;
+
+    [Tooltip("Spawns in every fish on list X amount of times")]
+    [SerializeField] private int spawnAmount = 1;
 
     private void Awake()
     {
@@ -24,19 +25,23 @@ public class FishSpawner : MonoBehaviour
 
     private void PlaceFishInGameBounds()
     {
-        for (int i = 0; i < fishPrefabs.Length; i++)
+        for (int j = 0; j < spawnAmount; j++)
         {
-            RandTarget = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            for (int i = 0; i < fishPrefabs.Length; i++)
+            {
+                RandTarget = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
 
-            RandTarget = RandTarget.normalized;
+                RandTarget = RandTarget.normalized;
 
-            RandTarget *= spawnRadius;
+                RandTarget *= spawnRadius;
 
-            Vector2 targetWorld = spawnerPoint.transform.TransformPoint(RandTarget);
-            fishPrefabs[i].transform.position = targetWorld;
-            fishPrefabs[i].transform.position = new Vector3(fishPrefabs[i].transform.position.x, fishPrefabs[i].transform.position.y, 0f);
-            Instantiate(fishPrefabs[i]);
-            
+                Vector2 targetWorld = spawnerPoint.transform.TransformPoint(RandTarget);
+                fishPrefabs[i].transform.position = targetWorld;
+                fishPrefabs[i].transform.position = new Vector3(fishPrefabs[i].transform.position.x, fishPrefabs[i].transform.position.y, 0f);
+                Instantiate(fishPrefabs[i]);
+
+            }
         }
+        
     }
 }
