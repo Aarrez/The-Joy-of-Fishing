4cf5ccb355f09d6fb2d0bbc7ab7925e6d0366a48
Leo Asplund <theleolevito@gmail.com>
2022-01-14 16:56:41 +0100

    Fixed UI not darkening when paused

--- Assets/Scripts/BoatScript.cs
+++ Assets/Scripts/BoatScript.cs
@@ -86,15 +86,15 @@ public class BoatScript : MonoBehaviour
         {
             currentlyReelingUp = false;
         }
-        if (reelfloatup == Vector2.up && ropeActive == true && elapsed >= 0.05f && rig2d.velocity.x.Equals(0) && boostbool == false)
+        if (reelfloatup == Vector2.up && ropeActive == true && elapsed >= 0.2f && rig2d.velocity.x.Equals(0) && boostbool == false)
         {
-            elapsed = elapsed % 0.05f; //polish would need floats in these statements varaibled so changeability with upgrades can be done.
+            elapsed = elapsed % 0.2f; //polish would need floats in these statements varaibled so changeability with upgrades can be done.
             RopeScript.instance.DestroyNode();
             currentlyReelingUp = true;
         }
-        else if (reelfloatup == Vector2.up && boostbool == true && elapsed >= 0.02f && ropeActive == true && rig2d.velocity.x.Equals(0))
+        else if (reelfloatup == Vector2.up && boostbool == true && elapsed >= 0.1f && ropeActive == true && rig2d.velocity.x.Equals(0))
         {
-            elapsed = elapsed % 0.02f;
+            elapsed = elapsed % 0.1f;
             RopeScript.instance.DestroyNode();
             currentlyReelingUp = true;
         }
