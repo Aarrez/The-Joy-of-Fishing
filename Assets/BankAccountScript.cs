e93ead191a1f37d381a8d3f3ceef82253e601e2d
Leo Asplund <theleolevito@gmail.com>
2022-01-20 16:54:11 +0100

    moving "you got this amount of money" beginnings

--- Assets/BankAccountScript.cs
+++ Assets/BankAccountScript.cs
@@ -9,16 +9,26 @@ public class BankAccountScript : MonoBehaviour
     TextMeshProUGUI BankText;
     public bool thisIs1;
     public bool thisIs2;
+    Rigidbody2D thisIs2rb;
+    Transform thisIs2transform;
+    float elapsed;
     void Start()
     {
         callMoneyEffectScript = FindObjectOfType<MoneyEffect>();
         BankText = GetComponent<TextMeshProUGUI>();
+        if (thisIs2)
+        {
+            thisIs2rb = GetComponent<Rigidbody2D>();
+            thisIs2transform = GetComponent<Transform>();
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (GameManager.instance.moveCam != 3)
+
+        if (GameManager.instance.moveCam != 3 && thisIs1)
         {
             BankText.text = "Dolluh$: " + callMoneyEffectScript.totalMoney.ToString() + "c";
         }
@@ -27,13 +37,32 @@ public class BankAccountScript : MonoBehaviour
         //    BankText.text = "";
         //}
 
-        //if (GameManager.instance.moveCam != 3 && thisIs2)
-        //{
-        //    BankText.text = "MoneyBank: " + callMoneyEffectScript.coinsBanked.ToString();
-        //}
+        if (GameManager.instance.moveCam != 3 && thisIs2)
+        {
+            elapsed += Time.deltaTime;
+            BankText.text = "Geebers";
+            thisIs2rb.velocity = new Vector2(0, 1);
+            if(elapsed >= 3 && elapsed <= 6)
+            {
+                thisIs2rb.velocity = new Vector2(0, -1);
+            }
+            if(elapsed > 6)
+            {
+                thisIs2rb.velocity = Vector2.zero;
+            }
+
+            Debug.Log("elapsed" + elapsed);
+        }
         ////else if (GameManager.instance.moveCam == 3)
         ////{
         ////    BankText.text = "";
         ////}
+    }
+
+    IEnumerator MoneyGainedTextVector()
+    {
+
+        yield return new WaitForSeconds(3);
+        thisIs2rb.velocity = Vector2.zero;
     }
 }
