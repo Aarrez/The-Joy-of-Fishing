cc930e5e0b311c52969607714beb4d64f7b3838b
Leo Asplund <theleolevito@gmail.com>
2022-01-20 15:55:46 +0100

    banktestinng commented off

--- Assets/BankAccountScript.cs
+++ Assets/BankAccountScript.cs
@@ -18,22 +18,22 @@ public class BankAccountScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(GameManager.instance.moveCam != 3 && thisIs1)
-        {
-            BankText.text = "MoneyGane: " + callMoneyEffectScript.coinsGained.ToString();
-        }
-        //else if(GameManager.instance.moveCam == 3)
+        //if(GameManager.instance.moveCam != 3 && thisIs1)
         //{
-        //    BankText.text = "";
+        //    BankText.text = "MoneyGane: " + callMoneyEffectScript.coinsGained.ToString();
         //}
+        ////else if(GameManager.instance.moveCam == 3)
+        ////{
+        ////    BankText.text = "";
+        ////}
 
-        if (GameManager.instance.moveCam != 3 && thisIs2)
-        {
-            BankText.text = "MoneyBank: " + callMoneyEffectScript.coinsBanked.ToString();
-        }
-        //else if (GameManager.instance.moveCam == 3)
+        //if (GameManager.instance.moveCam != 3 && thisIs2)
         //{
-        //    BankText.text = "";
+        //    BankText.text = "MoneyBank: " + callMoneyEffectScript.coinsBanked.ToString();
         //}
+        ////else if (GameManager.instance.moveCam == 3)
+        ////{
+        ////    BankText.text = "";
+        ////}
     }
 }
