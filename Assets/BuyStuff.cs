7516088116c21ea22f93bc329650827b167703f5
staledonuts <goblinburrow@gmail.com>
2022-01-22 10:00:40 +0100

    Store Implementation part 2

--- Assets/BuyStuff.cs
+++ Assets/BuyStuff.cs
@@ -8,11 +8,14 @@ public class BuyStuff : MonoBehaviour
 {
     public Button mindcontrol, lvl1Line, lvl2Line, lvl3Line;
     public uint mindControlCost, lvl1Cost, lvl2Cost, lvl3Cost;
+    public int lvl1Length, lvl2Length, lvl3Lenght;
     MoneyEffect callMoneyEffectScript;
+    BoatScript callBoatScript;
 
     private void Start() 
     {
-    callMoneyEffectScript = FindObjectOfType<MoneyEffect>();    
+    callMoneyEffectScript = FindObjectOfType<MoneyEffect>();   
+    callBoatScript = FindObjectOfType<BoatScript>(); 
     }
 
     public void BuyMindControlLure()
@@ -20,7 +23,6 @@ public class BuyStuff : MonoBehaviour
         if (callMoneyEffectScript.totalMoney >= mindControlCost)
         {
             mindcontrol.interactable = false;
-            GameManager.instance.currentLineLevel = 0;
             callMoneyEffectScript.totalMoney = callMoneyEffectScript.totalMoney - mindControlCost;
         }
         else
@@ -35,7 +37,7 @@ public class BuyStuff : MonoBehaviour
         if (callMoneyEffectScript.totalMoney >= lvl1Cost)
         {
             lvl1Line.interactable = false;
-            GameManager.instance.currentLineLevel = 1;
+            callBoatScript.maxLineLength = lvl1Length;
             callMoneyEffectScript.totalMoney = callMoneyEffectScript.totalMoney - lvl1Cost;
         }
         else
@@ -46,12 +48,33 @@ public class BuyStuff : MonoBehaviour
 
     public void BuyLineLVL2()
     {
-        
+        if (callMoneyEffectScript.totalMoney >= lvl2Cost)
+        {
+            lvl1Line.interactable = false;
+            lvl2Line.interactable = false;
+            callBoatScript.maxLineLength = lvl2Length;
+            callMoneyEffectScript.totalMoney = callMoneyEffectScript.totalMoney - lvl1Cost;
+        }
+        else
+        {
+            Debug.Log("not enough money!");
+        }
     }
 
     public void BuyLineLVL3()
     {
-        
+        if (callMoneyEffectScript.totalMoney >= lvl3Cost)
+        {
+            lvl1Line.interactable = false;
+            lvl2Line.interactable = false;
+            lvl3Line.interactable = false;
+            callBoatScript.maxLineLength = lvl3Lenght;
+            callMoneyEffectScript.totalMoney = callMoneyEffectScript.totalMoney - lvl1Cost;
+        }
+        else
+        {
+            Debug.Log("not enough money!");
+        }
     }
     
 }
