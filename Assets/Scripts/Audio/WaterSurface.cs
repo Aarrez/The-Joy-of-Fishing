b037ec94642b8b25d288173d54aaaa8427d213ff
Warlock Stones <warlockstones@protonmail.com>
2022-01-27 19:57:29 +0100

    fixed ambience + splash sfx

--- Assets/Scripts/Audio/WaterSurface.cs
+++ Assets/Scripts/Audio/WaterSurface.cs
@@ -1,44 +1,35 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class WaterSurface : MonoBehaviour
 {
-   [SerializeField] private bool hookSubmerged = false; //Hook is under the watersurface
+    [SerializeField] private bool hookSubmerged = false; //Hook is under the watersurface
 
     private FMOD.Studio.EventInstance splashEvent;
-    private BoatSliderScript boatSliderScript;
-
     private BoatEmitter boatEmitter;
-    
+
     //water ambience MOVE ME TO BETTER PLACE LATER?
     private FMOD.Studio.EventInstance lakeAmbienceEvent;
 
     private void Awake()
     {
-        boatSliderScript = FindObjectOfType<BoatSliderScript>();
         boatEmitter = FindObjectOfType<BoatEmitter>();
-        
+
         splashEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
         splashEvent = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/hook_splash");
+        splashEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
         //splashEvent.start();
-        
+
         lakeAmbienceEvent = FMODUnity.RuntimeManager.CreateInstance("event:/Ambience/ambience_lake");
+        lakeAmbienceEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(boatEmitter.transform.position));
         lakeAmbienceEvent.start();
-        
-        
     }
 
     private void Update()
     {
-        if (boatSliderScript.depthMathTotal < -0.5f)
-        {
-            hookSubmerged = true;
-        }
-        else hookSubmerged = false;
-        
-        
         if (hookSubmerged)
         {
             lakeAmbienceEvent.setParameterByName("music_duck", 1);
@@ -53,32 +44,27 @@ public class WaterSurface : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other) //WaterSurface need to be BaitLayer. This ok?
     {
-        if (other.gameObject.CompareTag("Bait") && boatSliderScript.depthMathTotal > -0.5f)
+        if (other.gameObject.CompareTag("Bait"))
         {
-            // For play lake ambience, maybe move me lateR?
-            //hookSubmerged = !hookSubmerged; //swap bool mode BROKEN
-            
-            
-             
-            
-            
-            
-            //Play FMOD Splash if not already playing to avoid duplicates
-            FMOD.Studio.PLAYBACK_STATE pbState;
-            splashEvent.getPlaybackState(out pbState);
-            if (pbState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+            if (other.transform.position.y > gameObject.transform.position.y)
             {
-                //FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/hook_splash", other.gameObject.transform.position);
-                // splashEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(other.gameObject));
-                // FMODUnity.RuntimeManager.AttachInstanceToGameObject(splashEvent, other.gameObject.transform);
-                
-                // Using Attatch the sound will stay on target.
-                // To3DAttributes is like playOneShot. It is at that position once. not updated
-                
-                splashEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(other.gameObject));
-                splashEvent.start();
-               
-                // No need to release this from memory. It is 1 sound played once at times.
+                hookSubmerged = true;
+
+
+                //Play FMOD Splash if not already playing to avoid duplicates
+                FMOD.Studio.PLAYBACK_STATE pbState;
+                splashEvent.getPlaybackState(out pbState);
+                if (pbState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+                {
+                    splashEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(other.gameObject));
+                    splashEvent.start();
+
+                    // No need to release this from memory. It is 1 sound played once at times.
+                }
+            }
+            else if (other.transform.position.y < gameObject.transform.position.y)
+            {
+                hookSubmerged = false;
             }
         }
     }
